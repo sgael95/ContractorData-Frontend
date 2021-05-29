ed107a978bbbcff7475531b38c1640b72b40aa58
sgael95 <sgael95@gmail.com>
2021-05-28 20:46:52 -0500

    Home screen background added.

---
+++ src/components/Home.jsx
@@ -0,0 +1,13 @@
+const Home = () => {
+
+
+    return(
+        <div className="home-page-container">
+            <div className="home-text-container">
+                <h1 className="home-text">Home Page</h1>
+            </div>
+        </div>
+    );
+}
+
+export default Home;
